c2363fdfed606d12047995ad4fb76d7a2907d885
lentinic <lentinic@gmail.com>
2017-09-03 00:28:00 -0700

    Test project, skeleton shell, client script using a socketio library to connect and join a game

---
+++ week12/client/tower_client/Assets/Client.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Quobject.SocketIoClientDotNet.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+public class Client : MonoBehaviour
+{ 
+    public void OnQuickPlay()
+    {
+        Debug.Log("Connecting");
+        Socket socket = IO.Socket("http://127.0.0.1");
+        socket.On(Socket.EVENT_CONNECT, () => {
+            Debug.Log("Registering");
+            socket.Emit("register", "");
+            socket.On("registered", (evt) => {
+                Debug.Log("Authenticating");
+                socket.Emit("authenticate", JObject.Parse("{ token: '" + ((JObject)evt)["token"] + "' }"));
+            });
+        });
+        socket.On("authenticated", (evt) => {
+            Debug.Log("Joining Lobby");
+            socket.Emit("lobby:join");
+        });
+        socket.On("lobby:joined", (evt) => {
+            Debug.Log("Joined Lobby");
+        });
+        socket.On("lobby:matched", (evt) => {
+            Debug.Log("matched");
+            socket.Emit("lobby:acceptGame");
+        });
+        socket.On("lobby:accepted", (evt) => {
+            Debug.Log("Accepted Game");
+        });
+        socket.On("lobby:partnerAccepted", (evt) => {
+            Debug.Log("Partner accepted gamed");
+        });
+        socket.On("lobby:partnerLeft", () => {
+            Debug.Log("Partner left game");
+        });
+        socket.On("game:starting", () => {
+            Debug.Log("Game starting");
+        });
+        socket.On("game:started", () => {
+            Debug.Log("Game started");
+        });
+        socket.On("game:state", (evt) => {
+            Debug.Log(evt.ToString());
+        });
+        socket.On("unauthorized", () => {
+            Debug.Log("Unauthorized");
+        });
+    }
+}
---
+++ week12/client/tower_client/Assets/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+    void Start()
+    {
+	}
+	
+	void Update()
+    {
+	}
+
+    public void OnQuickPlay()
+    {
+        UnityEngine.Debug.Log("OnQuickPlay");
+        gameObject.SetActive(false);
+    }
+
+}
