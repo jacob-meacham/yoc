bda01538f53fb85bf76e2c86f3d5788d35038c8a
Jacob Meacham <jacob.e.meacham@gmail.com>
2017-12-31 23:45:41 -0800

    Simple scaffolding for site.

---
+++ week16/first/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template
+from cryptozoo.models import markov
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+# with open("corpus/TableCaptions.txt") as f:
+#     text = f.read()
+#
+# model = markov.build_model(text, True)
+
+@app.route('/')
+def index():
+    return render_template('layout.html', **{
+        'title': 'Welcome',
+        'base_url': 'localhost:5000'
+    })
+
+@app.route('/submissions')
+def submissions():
+    return render_template('layout.html', **{
+        'title': 'Welcome',
+        'base_url': 'localhost:5000'
+    })
+
+@app.route('/program')
+def program():
+    return render_template('layout.html', **{
+        'title': 'Welcome',
+        'base_url': 'localhost:5000'
+    })
+
+@app.route('/program/award')
+def award():
+    return render_template('layout.html', **{
+        'title': 'Welcome',
+        'base_url': 'localhost:5000'
+    })
+
+@app.route('/program/paper/<paper>')
+def paper():
+    return render_template('layout.html', **{
+        'title': 'Welcome',
+        'base_url': 'localhost:5000'
+    })
+
+@app.route('/about')
+def paper():
+    return render_template('layout.html', **{
+        'title': 'About',
+        'base_url': 'localhost:5000'
+    })
